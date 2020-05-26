27456d76fd374abde21c785ce64e66395a58e6b9
Karlson <m.norets@gmail.com>
2020-05-26 13:35:27 +0300

    create firebase/store

--- toolkit_practice/src/App.js
+++ toolkit_practice/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch, Link } from "react-router-dom";
 import { BirthdayPage } from "./pages/BirthdayPage";
 import { PlanningPage } from "./pages/PlanningPage";
 import { FriendsWishesPage } from "./pages/FriendsWishesPage";
-
+import { BirthdayBoyPage } from "./pages/BirthdayBoyPage";
 // import {} from "./pages/BuyPage";
 // import {} from "./pages/TravelPage";
 
@@ -17,6 +17,7 @@ function App() {
       </ul>
       <Switch>
         <Route exact path="/" component={PlanningPage} />
+        <Route path="/birthday/birthdayBoy" component={BirthdayBoyPage} />
         <Route path="/birthday/:friends" component={FriendsWishesPage} />
         <Route path="/birthday" component={BirthdayPage} />
       </Switch>
--- toolkit_practice/src/pages/BirthdayPage.js
+++ toolkit_practice/src/pages/BirthdayPage.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { firestore } from "../firebase/config";
 
 export const BirthdayPage = () => {
+  const [state, setstate] = useState([]);
+
+  useEffect(() => {
+    currentBirthdayBoy();
+  }, []);
+
+  // useEffect(() => {
+  //   currentBirthdayBoyWishes();
+  // }, []);
+
+  const currentBirthdayBoy = () => {
+    firestore
+      .collection("birthday")
+      .onSnapshot((doc) =>
+        setstate(doc.docs.map((d) => ({ ...d.data(), wishId: d.id })))
+      );
+  };
+
+  const currentBirthdayBoyWishes = (id) => {
+    firestore
+      .collection("birthday")
+      .doc(id)
+      .collection("wishes")
+      .onSnapshot((doc) =>
+        console.log(doc.docs.map((d) => ({ ...d.data(), wishId: d.id })))
+      );
+  };
+
   return (
     <div>
       <h2>BirthdayPage</h2>
+      <ul>
+        {state.map((b) => (
+          <li key={b.wishId}>
+            <h2>{b.birdayBoyName}</h2>
+            <button onClick={() => currentBirthdayBoyWishes(b.wishId)}>
+              show wish
+            </button>
+          </li>
+        ))}
+      </ul>
+      <Link to="/birthday/birthdayBoy">birthdayBoy</Link>
     </div>
   );
 };
